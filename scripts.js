98a59db0e3b9e58f2c658f3473efc31ae50b80f1
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-08-02 23:49:43 +0400

    added hover effect

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-const gridContainer = document.querySelector('.grid-16');
-const targets = document.querySelectorAll(".grid-box");
-const canvasSize = document.querySelector('.size');
+const gridContainer = document.querySelector('.grid');
 
 for (let each = 0;  each < 256; each++) {
     const div = document.createElement('div');
@@ -9,27 +7,19 @@ for (let each = 0;  each < 256; each++) {
     gridContainer.append(div);
 }
 
+// const divs = document.querySelectorAll('.grid-box');
+// divs.hover()
+
+const targets = document.querySelectorAll(".grid-box");
 targets.forEach(element => {
     element.addEventListener('mouseover', () => {
         element.classList.add('hover');
     })
+
 })
 
-canvasSize.addEventListener('click', () => {
-    let userInput = prompt('enter size');
-    console.log(userInput);
-    var child = gridContainer.lastElementChild;
-    while (child) {
-        gridContainer.removeChild(child);
-        child = gridContainer.lastElementChild;
-    }
-
-    gridContainer.setAttribute("style", `grid-template-columns: repeat(${userInput}, auto);`);
-
-    for (let each = 0;  each < userInput**2; each++) {
-        const newDiv = document.createElement('div');
-        newDiv.textContent = `${each+1}`;
-        newDiv.classList.add('grid-box');
-        gridContainer.append(newDiv);
-    }
-});
+function mOver(x) {
+    targets.forEach(element => {
+        element.setAttribute("style", "background-color:blue;");
+    })
+}
